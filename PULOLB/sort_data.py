475cce70f3f25ee8c850e538954650237bdfd546
Rossana <rossana13@D-69-91-172-175.dhcp4.washington.edu>
2018-03-03 15:07:17 -0800

    Sorting package

--- PULOLB/sort_data.py
+++ PULOLB/sort_data.py
@@ -2,12 +2,15 @@ import pandas as pd
 import import_data
 
 def by_cycle(filename):
+    """This function sorts the data by the cycle number"""
     
     df = import_data.single_pd_matlab_data(filename)
     
+    #Initialize a dictionary for the cycle number
     cycle_dict = {}
     cycle_list = df['cycle'].unique()
 
+    #Add the data in the dictonary so that the keys are the cycle numbers
     for idx in list(cycle_list):
         if idx not in cycle_dict.keys():
             cycle_dict[idx] = df.loc[df['cycle'] == idx]
@@ -17,12 +20,16 @@ def by_cycle(filename):
     return cycle_dict    
 
 def charge_discharge(filename):
+    """This function separates the data into charge and discharge cycles"""
     
+    #Creat the data dictionary sorted by cycle number
     cycle_dict = by_cycle(filename)
     
+    #Initialize dictionaries for charge and discharge
     charge_dict = {}
     discharge_dict = {}
     
+    #Add the data into the dictionaries
     for idx in list(cycle_dict.keys()):
         if idx not in charge_dict.keys():
             charge_dict[idx] = cycle_dict[idx].loc[cycle_dict[idx]['current_amp'] > 0]
