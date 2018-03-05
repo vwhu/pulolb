71ea27ccdad77aa1973072c358d9ca0d2a467b9b
Rossana <rossana13@Rossanas-MacBook-Pro.local>
2018-03-04 22:53:29 -0800

    Update 04032018

--- PULOLB/import_data.py
+++ PULOLB/import_data.py
@@ -2,55 +2,14 @@ import hdf5storage
 import numpy as np
 import pandas as pd
 
-<<<<<<< HEAD
-def import_matlab_data(filename):
-    """Taking a Matlab file of Arbin data from the CALCE database and imports it into a host of different NumPy arrays
-    Filename: 
-    The .mat file that needs to be converted to a pandas dataframe. """
-=======
-
 def import_matlab_data(filename):
     """Taking a Matlab file of Arbin data from the CALCE database and imports it into a host of different NumPy arrays
         Filename: Matlab file 
     """
->>>>>>> fb5ff4f16d603336475778cc5578192101688b8c
     
     #Importing the file as a HDF5 dataframe, stored in the form of a dictionary.  
     df = hdf5storage.loadmat(filename)
     return df
-
-<<<<<<< HEAD
-def single_pd_matlab_data(filename):
-	""" Unpacks the .mat data file for readibility as a pandas dataframe. 
-	Filename: 
-    The .mat file that needs to be converted to a pandas dataframe. """
-    
-    #Importing the file. 
-    df = import_matlab_data(filename)
-    keys = df.keys()
-
-    #Changing the filename in the dictionary to data. 
-    for i in keys:
-            if i == '#subsystem#':
-                pass
-            else:
-                data = i
-
-    #Calling each column inside the file, and assigning column names. 
-    time_sum = df['#subsystem#'][0][0][0][2][0][0].flatten()
-    datetime_sum = df['#subsystem#'][0][0][0][2][0][1].flatten()
-    step_sum = df['#subsystem#'][0][0][0][2][0][2].flatten()
-    cycle_sum = df['#subsystem#'][0][0][0][2][0][3].flatten()
-    current_sum = df['#subsystem#'][0][0][0][2][0][4].flatten()
-    voltage_sum = df['#subsystem#'][0][0][0][2][0][5].flatten()
-    charge_ah_sum = df['#subsystem#'][0][0][0][2][0][6].flatten()
-    discharge_ah_sum = df['#subsystem#'][0][0][0][2][0][7].flatten()
-    count = 9
-
- 	#To access the number of cycles in each dataset. 
-    for idx in range(1, len(df[data])): 
-        #For missing data. 
-=======
 
 def initialize_datasets(filename):
     """This function initializes the columns of the dataframe. 
@@ -121,31 +80,24 @@ def single_pd_matlab_data(filename):
         length_test = (df['#subsystem#'][0][0][0][count][0][0].flatten())
          
         #For missing data: Store the number of cycles with missing data and add them up into the cycles count
->>>>>>> fb5ff4f16d603336475778cc5578192101688b8c
         if df[data][idx][2].shape == (0,0):
             name = df[data][idx][0][0][0]
             name = name.split(' ')
             number = float(name[0])
             cycle_sum = np.append(cycle_sum, (cycle_sum[-1] + number))
+
         #For single cycles: Ignore cycles with less than 3500 values.
         elif len(length_test) < 3500:
             count = count + 7
         #Extract data from each column
-        else:
-        	#Concatenating all the data for one dataset with all the cycle operations for easy reference. 
+        else: 
             time_idx = (df['#subsystem#'][0][0][0][count][0][0].flatten()) + time_sum[-1]
             datetime_idx = df['#subsystem#'][0][0][0][count][0][1].flatten()
-            
-
             step_idx = df['#subsystem#'][0][0][0][count][0][2].flatten()
-<<<<<<< HEAD
-            
-            #To concatenate the cycle data, 
-=======
-            step_sum = np.concatenate((step_sum, step_idx), axis = 0)
+
 
             #For the cycle number, evaluate if there were missing data.
->>>>>>> fb5ff4f16d603336475778cc5578192101688b8c
+
             if len(cycle_sum) > len(current_sum):
                 diff = len(cycle_sum) - len(current_sum)
                 cycle_prior = cycle_sum[-1]
@@ -157,11 +109,13 @@ def single_pd_matlab_data(filename):
                 cycle_prior = cycle_sum[-1]
                 cycle_idx = (df['#subsystem#'][0][0][0][count][0][3].flatten()) + cycle_prior
                 cycle_test = (df['#subsystem#'][0][0][0][count][0][3].flatten())              
+            
             current_idx = df['#subsystem#'][0][0][0][count][0][4].flatten()
             voltage_idx = df['#subsystem#'][0][0][0][count][0][5].flatten()
             charge_ah_idx = df['#subsystem#'][0][0][0][count][0][6].flatten()
             discharge_ah_idx = df['#subsystem#'][0][0][0][count][0][7].flatten()
 
+            #Delete the last curve of the set for each experiment (since it is a single full cycle)
             min_ = np.unique(cycle_idx).min()
             max_ = np.unique(cycle_idx).max()
             idx_for_start = np.argwhere(cycle_idx == min_)[-1]
@@ -179,6 +133,7 @@ def single_pd_matlab_data(filename):
             
             count = count + 7
             
+            #Concatenate the extracted data
             time_sum = np.concatenate((time_sum, time_idx), axis = 0)
             datetime_sum = np.concatenate((datetime_sum, datetime_idx), axis = 0)
             step_sum = np.concatenate((step_sum, step_idx), axis = 0)
@@ -190,10 +145,7 @@ def single_pd_matlab_data(filename):
             if count > (len(df['#subsystem#'][0][0][0])-7):
                 break
 
-<<<<<<< HEAD
-=======
     #Creat a DataFrame from the arrays for each column             
->>>>>>> fb5ff4f16d603336475778cc5578192101688b8c
     host_df = pd.DataFrame()
 
     host_df['time'] = time_sum
