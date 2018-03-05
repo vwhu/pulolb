0689d83a5c73d165929e282999803c432131c9ed
Maitri Uppaluri <maitriu@uw.edu>
2018-03-04 21:22:05 -0800

    Added changes in index formation.

--- PULOLB/import_data.py
+++ PULOLB/import_data.py
@@ -1,28 +1,33 @@
+import hdf5storage
+import numpy as np
+import pandas as pd
+
 def import_matlab_data(filename):
-    """Taking a Matlab file of Arbin data from the CALCE database and imports it into a host of different NumPy arrays"""
-    
-    import hdf5storage
-    import numpy as np
-    import pandas as pd
+    """Taking a Matlab file of Arbin data from the CALCE database and imports it into a host of different NumPy arrays
+    Filename: 
+    The .mat file that needs to be converted to a pandas dataframe. """
     
     #Importing the file as a HDF5 dataframe, stored in the form of a dictionary.  
     df = hdf5storage.loadmat(filename)
     return df
 
 def single_pd_matlab_data(filename):
+	""" Unpacks the .mat data file for readibility as a pandas dataframe. 
+	Filename: 
+    The .mat file that needs to be converted to a pandas dataframe. """
     
-    import numpy as np
-    import pandas as pd
-    
+    #Importing the file. 
     df = import_matlab_data(filename)
     keys = df.keys()
 
+    #Changing the filename in the dictionary to data. 
     for i in keys:
             if i == '#subsystem#':
                 pass
             else:
                 data = i
 
+    #Calling each column inside the file, and assigning column names. 
     time_sum = df['#subsystem#'][0][0][0][2][0][0].flatten()
     datetime_sum = df['#subsystem#'][0][0][0][2][0][1].flatten()
     step_sum = df['#subsystem#'][0][0][0][2][0][2].flatten()
@@ -33,9 +38,8 @@ def single_pd_matlab_data(filename):
     discharge_ah_sum = df['#subsystem#'][0][0][0][2][0][7].flatten()
     count = 9
 
-    for idx in range(1, len(df[data])):
-        #To access the number of cycles in each dataset. 
-        
+ 	#To access the number of cycles in each dataset. 
+    for idx in range(1, len(df[data])): 
         #For missing data. 
         if df[data][idx][2].shape == (0,0):
             name = df[data][idx][0][0][0]
@@ -43,46 +47,57 @@ def single_pd_matlab_data(filename):
             number = float(name[0])
             cycle_sum = np.append(cycle_sum, (cycle_sum[-1] + number)) 
         else:
+        	#Concatenating all the data for one dataset with all the cycle operations for easy reference. 
             time_idx = (df['#subsystem#'][0][0][0][count][0][0].flatten()) + time_sum[-1]
-            time_sum = np.concatenate((time_sum, time_idx), axis = 0)
-
             datetime_idx = df['#subsystem#'][0][0][0][count][0][1].flatten()
-            datetime_sum = np.concatenate((datetime_sum, datetime_idx), axis = 0)
+            
 
             step_idx = df['#subsystem#'][0][0][0][count][0][2].flatten()
-            step_sum = np.concatenate((step_sum, step_idx), axis = 0)
-
+            
+            #To concatenate the cycle data, 
             if len(cycle_sum) > len(current_sum):
                 diff = len(cycle_sum) - len(current_sum)
                 cycle_prior = cycle_sum[-1]
                 for i in range(0,diff):
                     cycle_sum = np.delete(cycle_sum, -1)
                 cycle_idx = (df['#subsystem#'][0][0][0][count][0][3].flatten()) + cycle_prior
-                cycle_test = (df['#subsystem#'][0][0][0][count][0][3].flatten())
-                cycle_sum = np.concatenate((cycle_sum, cycle_idx), axis = 0)
+                cycle_test = (df['#subsystem#'][0][0][0][count][0][3].flatten())    
             else:
                 cycle_prior = cycle_sum[-1]
                 cycle_idx = (df['#subsystem#'][0][0][0][count][0][3].flatten()) + cycle_prior
-                cycle_test = (df['#subsystem#'][0][0][0][count][0][3].flatten())
-                cycle_sum = np.concatenate((cycle_sum, cycle_idx), axis = 0)
-
+                cycle_test = (df['#subsystem#'][0][0][0][count][0][3].flatten())              
             current_idx = df['#subsystem#'][0][0][0][count][0][4].flatten()
-            current_sum = np.concatenate((current_sum, current_idx), axis = 0)
-
             voltage_idx = df['#subsystem#'][0][0][0][count][0][5].flatten()
-            voltage_sum = np.concatenate((voltage_sum, voltage_idx), axis = 0)
-
             charge_ah_idx = df['#subsystem#'][0][0][0][count][0][6].flatten()
-            charge_ah_sum = np.concatenate((charge_ah_sum, charge_ah_idx), axis = 0)
-
             discharge_ah_idx = df['#subsystem#'][0][0][0][count][0][7].flatten()
-            discharge_ah_sum = np.concatenate((discharge_ah_sum, discharge_ah_idx), axis = 0)
 
+            min_ = np.unique(cycle_idx).min()
+            max_ = np.unique(cycle_idx).max()
+            idx_for_start = np.argwhere(cycle_idx == min_)[-1]
+            idx_for_deletion = np.argwhere(cycle_idx == max_)[0]
+
+            time_idx = time_idx[int(idx_for_start + 1):int(idx_for_deletion - 1)]
+            datetime_idx = datetime_idx[int(idx_for_start + 1):int(idx_for_deletion - 1)]
+            step_idx = step_idx[int(idx_for_start + 1):int(idx_for_deletion - 1)]
+            cycle_idx = cycle_idx[int(idx_for_start + 1):int(idx_for_deletion - 1)]
+            current_idx = current_idx[int(idx_for_start + 1):int(idx_for_deletion - 1)]
+            voltage_idx = voltage_idx[int(idx_for_start + 1):int(idx_for_deletion - 1)]
+            charge_ah_idx = charge_ah_idx[int(idx_for_start + 1):int(idx_for_deletion - 1)]
+            discharge_ah_idx = discharge_ah_idx[int(idx_for_start + 1):int(idx_for_deletion - 1)]
+        
+            
             count = count + 7
-
+            
+            time_sum = np.concatenate((time_sum, time_idx), axis = 0)
+            datetime_sum = np.concatenate((datetime_sum, datetime_idx), axis = 0)
+            step_sum = np.concatenate((step_sum, step_idx), axis = 0)
+            cycle_sum = np.concatenate((cycle_sum, cycle_idx), axis = 0)
+            current_sum = np.concatenate((current_sum, current_idx), axis = 0)
+            voltage_sum = np.concatenate((voltage_sum, voltage_idx), axis = 0)
+            charge_ah_sum = np.concatenate((charge_ah_sum, charge_ah_idx), axis = 0)
+            discharge_ah_sum = np.concatenate((discharge_ah_sum, discharge_ah_idx), axis = 0)
             if count > (len(df['#subsystem#'][0][0][0])-7):
                 break
-
 
     host_df = pd.DataFrame()
 
