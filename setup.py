8b80f66fbcd4974e81422a520785c1e2a617ef82
Victor Hu <vwhu@uw.edu>
2018-03-11 20:50:14 -0700

    Cleaning up the whole repo

--- setup.py
+++ setup.py
@@ -3,27 +3,26 @@ from setuptools import setup, find_packages
 PACKAGES = find_packages()
 
 # Get version and release info, which is all stored in shablona/version.py
-ver_file = os.path.join('shablona', 'version.py')
-with open(ver_file) as f:
-    exec(f.read())
+#ver_file = os.path.join('shablona', 'version.py')
+#with open(ver_file) as f:
+#    exec(f.read())
 
-opts = dict(name=NAME,
-            maintainer=MAINTAINER,
-            maintainer_email=MAINTAINER_EMAIL,
-            description=DESCRIPTION,
-            long_description=LONG_DESCRIPTION,
-            url=URL,
-            download_url=DOWNLOAD_URL,
-            license=LICENSE,
-            classifiers=CLASSIFIERS,
-            author=AUTHOR,
-            author_email=AUTHOR_EMAIL,
-            platforms=PLATFORMS,
-            version=VERSION,
-            packages=PACKAGES,
-            package_data=PACKAGE_DATA,
-            install_requires=REQUIRES,
-            requires=REQUIRES)
+opts = dict(name='pulolb',
+            maintainer='Victor Hu, Yusong Liu, Rossana Scavone, Maitri Uppaluri',
+            maintainer_email='vwhu@uw.edu,',
+            description='Predicting remaining usable lifetime of lithium-ion batteries',
+            url='https://github.com/vwhu/PULOLB',
+            download_url='DOWNLOAD_URL',
+            license='MIT License',
+            classifiers='3 - Alpha',
+            author='Victor Hu, Yusong Liu, Rossana Scavone, Maitri Uppaluri',
+            author_email='vwhu@uw.edu,',
+            platforms='PLATFORMS',
+            version='0.0.1',
+            packages=find_packages(),
+            package_data='PACKAGE_DATA',
+            install_requires=['numpy', 'pandas', 'h5py', 'hdf5storage', 'fastdtw' ,'scipy', 'matplotlib'],
+            requires='REQUIRES')
 
 
 if __name__ == '__main__':
